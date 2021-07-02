5e2451a2288d9c7e30a4abdc5d1cf0a2d9e00b6f
aisar.nazmi <aisar.nazmi@teknomatrik.com.my>
2021-07-02 19:50:05 +0800

    modify user model, migrate and seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,35 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = [
+            [
+                'name' => 'Admin',
+                'email' => 'admin@onlinewebtutorblog.com',
+                'is_admin' => '1',
+                'password' => bcrypt('123456'),
+            ],
+            [
+                'name' => 'User',
+                'email' => 'normal@onlinewebtutorblog.com',
+                'is_admin' => '0',
+                'password' => bcrypt('123456'),
+            ],
+        ];
+
+        foreach ($user as $key => $value) {
+            User::create($value);
+        }
     }
 }
